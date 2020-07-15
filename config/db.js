56652b1e4fd16d63886c3637c3e6f0b6dbfe6f66
Zeha Irawan <jangkaringbumi@gmail.com>
2020-07-15 19:07:08 +0700

    connecting to local db

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize');
+const config = require('config');
+
+const db = config.get('postgresURL');
+
+const sequelize = new Sequelize(db);
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+module.exports = connectDB;
